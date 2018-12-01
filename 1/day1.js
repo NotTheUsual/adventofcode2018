c432a80fd3604cab0c736e9837cdbca3de4ced7a
James Hunter <james.hunter@starlingbank.com>
2018-12-01 12:03:22 +0000

    Day 1a

---
+++ 1/day1.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+
+function parse(input) {
+  return input
+    .split('\n')
+    .reduce((sum, instruction) => sum + Number(instruction), 0)
+}
+
+function test(input, expected) {
+  const result = parse(input)
+  const symbol = (result === expected) ? '✅' : '❌'
+  console.log(symbol, result, expected)
+}
+
+function run() {
+  const input = fs.readFileSync('./input1a.txt', 'utf8')
+  const result = parse(input)
+  console.log(`=================
+  ${result}
+=================`)
+}
+
+test(`+1
++1
++1`
+,3)
+
+test(`+1
++1
+-2`
+,0)
+
+test(`-1
+-2
+-3`
+,-6)
+
+run()
